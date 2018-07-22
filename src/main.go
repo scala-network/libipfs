48c583c40fa99b26c8c50374d5d58b19d184e5ec
Donovan Solms <donovan.solms@gmail.com>
2018-07-22 14:22:28 +0200

    Initial commit

---
+++ src/main.go
@@ -0,0 +1,11 @@
+// package main implements libznipfs as an executable instead of a library
+package main
+
+import "fmt"
+
+// main runs libznipfs and retrieves test data from ZeroNet and IPFS
+func main() {
+
+	fmt.Println("Test libznipfs as executable")
+
+}
