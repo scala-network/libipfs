51d2e64d7f7841e3675ba2cf8194ba0e063efce7
Donovan Solms <donovan.solms@gmail.com>
2018-07-22 18:37:04 +0200

    Added ZeroNet

---
+++ src/zeronet/zeronet.go
@@ -0,0 +1,57 @@
+package zeronet
+
+import (
+	"io/ioutil"
+	"os"
+
+	"github.com/donovansolms/ZeroGo/site_manager"
+	"github.com/donovansolms/ZeroGo/utils"
+
+	log "github.com/Sirupsen/logrus"
+)
+
+// ZeroNet contains the functionality to communicate with ZeroNet
+type ZeroNet struct {
+	dataPath    string
+	siteManager *site_manager.SiteManager
+}
+
+// New constructs and initializes ZeroNet and returns the instance
+func New(dataPath string) (*ZeroNet, error) {
+
+	// We discard log output to avoid the underlying libraries to print
+	// stuff out in the daemon
+	log.SetLevel(log.ErrorLevel)
+	log.SetOutput(ioutil.Discard)
+
+	// Set up ZeroNet
+	utils.SetDataPath(dataPath)
+	siteManager := site_manager.NewSiteManager()
+
+	// Create instance
+	zn := ZeroNet{
+		dataPath:    dataPath,
+		siteManager: siteManager,
+	}
+
+	return &zn, nil
+}
+
+// GetFile retrieves the given filename from the ZeroNet address
+// and returns the contents as bytes
+func (zn *ZeroNet) GetFile(address string, filename string) ([]byte, error) {
+	// Create the ZeroNet data path and add certificates
+	os.MkdirAll(utils.GetDataPath(), 0744)
+	utils.CreateCerts()
+
+	// Fetch the site from ZeroNet and wait for the operation to complete
+	site := zn.siteManager.Get(address)
+	site.Wait()
+
+	// Get the specified file from the ZeroNet address
+	data, err := site.GetFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return data, nil
+}
