26ea2c1f53541cddc494d99779b62c13b195d684
Donovan Solms <donovan.solms@gmail.com>
2019-06-18 21:38:27 +0200

    Cleaning house for new implementation

--- src/main.go
+++ src/main.go
@@ -2,11 +2,43 @@
 package main
 
 import (
-	shell "github.com/ipfs/go-ipfs-api"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/contribute-torque/libznipfs/src/zeronet"
+	//	shell "github.com/ipfs/go-ipfs-api"
 )
 
 // main runs libznipfs and retrieves test data from ZeroNet and IPFS
 func main() {
+
+	fmt.Println("Test libznipfs as executable")
+
+	baseDataPath := "/tmp"
+
+	zn, err := zeronet.New(filepath.Join(baseDataPath, "zn-test"))
+	if err != nil {
+		fmt.Printf("Unable to create ZeroNet instance: %s\n", err)
+		os.Exit(1)
+	}
+
+	// ZeroNet address 1KvWEyqhyHsU9y6UT8xYCFDC8Y1vKaNueX - Live JSON
+	// ZeroNet address 1CH9ApTd83RM8ggz35pZApnKoZqDf7wXyh - Testnet JSON
+	znAddress := "1KvWEyqhyHsU9y6UT8xYCFDC8Y1vKaNueX"
+
+	fmt.Println("Fetching file from ZeroNet:", znAddress)
+	// This is a well-known ZeroNet address. We store the IPFS hash in ipfs.hash
+	content, err := zn.GetFile(znAddress, "ipfs.hash")
+	if err != nil {
+		fmt.Printf("Unable fetch from ZeroNet: %s\n", err)
+		os.Exit(1)
+	}
+
+	// The content retrieved from ZeroNet is the IPFS hash containing the nodelist
+	ipfsHash := strings.TrimSpace(string(content))
+	fmt.Printf("Get IPFS hash from ZeroNet: %s\n", ipfsHash)
 
 	//
 	// NOTE Start IPFS extract and run daemon
@@ -53,11 +85,11 @@ func main() {
 	//
 	// NOTE: Fetch via IPFS API
 	//
-	sh := shell.NewShell("localhost:5001")
-	err := sh.Get("QmbQVPLwUSbLQL3tkQGFRFWDTCjRs6LSJDPEjG24BnNuhD", "/tmp/testfile.txt")
-	if err != nil {
-		panic(err)
-	}
+	// sh := shell.NewShell("localhost:5001")
+	// err := sh.Get("QmbQVPLwUSbLQL3tkQGFRFWDTCjRs6LSJDPEjG24BnNuhD", "/tmp/testfile.txt")
+	// if err != nil {
+	// 	panic(err)
+	// }
 	//
 	// NOTE: End Fetch via IPFS API
 	//
