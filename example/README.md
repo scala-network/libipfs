beffb22b3433d40f4166730e810ac74eebd190fd
hayzamjs <hayzam@gmail.com>
2021-02-19 11:27:24 +0530

    Different ports, startup dance, Cat command
    Added different ports just incase client machine already has an instance of IPFS running, let's not interfere with that, added a little startup dance to check if IPFS is up and running, Cat command will allow us to see files in folders or files themselves

--- example/README.md
+++ example/README.md
@@ -10,7 +10,7 @@
 ## Example output
 
 ```json
-{"Status":"ok","Message":"IPFS node started on port 5001"}
+{"Status":"ok","Message":"IPFS node started on port 11816"}
 {"Status":"ok","Message":"/ipfs/QmSgSaLtfnPrjU6BBKUrxZG93dAZhz1gEsMgW1GbY5ne1u"}
 {"Status":"ok","Message":"QmaXwmJd2XEFAGkmauKomK13mERtRC1h3Frmi1LgyADSHZ"}
 {"Status":"ok","Message":"IPFS node stopped"}
--- src/ipfs/ipfs.go
+++ src/ipfs/ipfs.go
@@ -3,11 +3,13 @@ package ipfs
 import (
 
 	"fmt"
+	"io"
 	"io/ioutil"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"runtime"
+	"errors"
 	"strings"
 	"time"
     "net/http"
@@ -94,8 +96,10 @@ func (ipfs *IPFS) Start() error {
 		// If we got an error that references that we need to 'ipfs init' first
 		// it most likely means this is a first run
 		if strings.Contains(string(op), "ipfs init") {
+			/* Init IPFS */
 			cmd = exec.Command(ipfs.daemonPath, "init", "--profile", "server")
 			cmd.Env = ipfsEnv
+
 			_, err := cmd.CombinedOutput()
 			if err != nil {
 				// If we hit this, ipfs could not init a new repo
@@ -106,6 +110,19 @@ func (ipfs *IPFS) Start() error {
 			return err
 		}
 	}
+
+	/* Set the correct port for IPFS daemon other than regular IPFS ones to limit interference */
+	cmdP2PPort := exec.Command(ipfs.daemonPath, "config", "--json", "Addresses.Swarm", "[\"/ip4/0.0.0.0/tcp/11814\",\"/ip6/::/tcp/11814\",\"/ip4/0.0.0.0/udp/11814/quic\",\"/ip6/::/udp/11814/quic\"]")
+	cmdP2PPort.Env = ipfsEnv
+	_ = cmdP2PPort.Run()
+
+	cmdGatewayPort := exec.Command(ipfs.daemonPath, "config", "Addresses.Gateway", "/ip4/127.0.0.1/tcp/11815")
+	cmdGatewayPort.Env = ipfsEnv
+	_ = cmdGatewayPort.Run()
+
+	cmdAPIPort := exec.Command(ipfs.daemonPath, "config", "Addresses.API", "/ip4/127.0.0.1/tcp/11816")
+	cmdAPIPort.Env = ipfsEnv
+	_ = cmdAPIPort.Run()
 
 	// Repo is good to go, we can start the daemon
 	ipfs.daemonCmd = exec.Command(ipfs.daemonPath, "daemon", "--enable-namesys-pubsub")
@@ -120,8 +137,26 @@ func (ipfs *IPFS) Start() error {
 		fmt.Println("IPFS daemon completed with exit:", err)
 	}()
 	// Give the daemon some time to start up
-	// TODO: I don't have a simple way to check this... yet
-	time.Sleep(time.Second * 5)
+	// TODO: I don't have a simple way to check this... yet - Donovan
+	// A better patch than just waiting, could be a bit more improved?
+
+	var failCount int = 0
+	
+	for i := 0; i <= 5; i++ {
+		sh := shell.NewShell("127.0.0.1:11816")
+		_, _, err := sh.Version()
+		
+		if err != nil {
+			if failCount >= 4{
+				err1 := errors.New("IPFS could not be started")
+				return err1
+			}
+			failCount = failCount + 1
+			time.Sleep(time.Second * 5)
+		} else {
+			return nil
+		}
+	}
 	return nil
 }
 
@@ -129,7 +164,7 @@ func (ipfs *IPFS) Start() error {
 func (ipfs *IPFS) Get(hash string) ([]byte, error) {
 
 	downloadPath := filepath.Join(ipfs.basePath, hash)
-	sh := shell.NewShell("localhost:5001")
+	sh := shell.NewShell("127.0.0.1:11816")
 	err := sh.Get(hash, downloadPath)
 	if err != nil {
 		return nil, err
@@ -140,7 +175,7 @@ func (ipfs *IPFS) Get(hash string) ([]byte, error) {
 // Resolve an IPNS name to IPFS hash
 func (ipfs *IPFS) Resolve(peerid string) (string, error) {
 
-	sh := shell.NewShell("localhost:5001")
+	sh := shell.NewShell("127.0.0.1:11816")
 	resp, err := sh.Resolve(peerid)
 	if err != nil {
 		return "", err
@@ -148,10 +183,30 @@ func (ipfs *IPFS) Resolve(peerid string) (string, error) {
 	return resp, nil
 }
 
+// Cat an IPFS file
+func (ipfs *IPFS) Cat(hash string) (string, error) {
+
+	sh:= shell.NewShell("127.0.0.1:11816")
+	resp, err:= sh.Cat(hash)
+
+	if err != nil {
+		return "", err
+	}
+
+	buf := new(strings.Builder)
+	_, err2 := io.Copy(buf, resp)
+
+	if err2 != nil {
+		return "", err2
+	}
+
+	return buf.String(), nil
+}
+
 // Add a directory to IPFS
 func (ipfs *IPFS) AddDirectory(directory string) (string, error) {
 
-	sh := shell.NewShell("localhost:5001")
+	sh := shell.NewShell("127.0.0.1:11816")
 	resp, err := sh.AddDir(directory)
 	if err != nil {
 		return "", err
@@ -162,7 +217,7 @@ func (ipfs *IPFS) AddDirectory(directory string) (string, error) {
 // Add bootstrap nodes to IPFS
 func (ipfs *IPFS) BootstrapAdd(peers []string) (string, error) {
 
-	sh := shell.NewShell("localhost:5001")
+	sh := shell.NewShell("127.0.0.1:11816")
 	resp, err := sh.BootstrapAdd(peers)
 	if err != nil {
 		return "", err
@@ -176,7 +231,7 @@ func (ipfs *IPFS) BootstrapAdd(peers []string) (string, error) {
 // Get PeerID
 func (ipfs *IPFS) GetPeerID() (string, error) {
     
-    sh:= shell.NewShell("localhost:5001")
+    sh:= shell.NewShell("127.0.0.1:11816")
     resp, err:= sh.ID();
     if err != nil {
         return "", err
@@ -186,7 +241,7 @@ func (ipfs *IPFS) GetPeerID() (string, error) {
 
 // Publish a content hash to IPNS
 func (ipfs *IPFS) PublishName(contentHash string) (string, error) {
-    sh:= shell.NewShell("localhost:5001")
+    sh:= shell.NewShell("127.0.0.1:11816")
     err:= sh.Publish("", contentHash);
     if err != nil {
         return "",err
@@ -199,7 +254,7 @@ func (ipfs *IPFS) PublishName(contentHash string) (string, error) {
 func (ipfs *IPFS) Stop() (error) {
 
     client := &http.Client{}
-    req, err := http.NewRequest("POST", "http://localhost:5001/api/v0/shutdown", nil)
+    req, err := http.NewRequest("POST", "http://127.0.0.1:11816/api/v0/shutdown", nil)
     if err != nil {
         return err
     }
