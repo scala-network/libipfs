7cf18f73ccda984d9979ca618bce253bac82ba4e
hayzamjs <hayzam@gmail.com>
2021-12-06 19:36:43 +0530

    Add bootstrap nodes for faster peer discovery

--- go.mod
+++ go.mod
@@ -8,4 +8,6 @@ require (
 	github.com/ipfs/go-ipfs-config v0.16.0 // indirect
 	github.com/ipfs/go-ipfs-files v0.0.8 // indirect
 	github.com/ipfs/interface-go-ipfs-core v0.5.1 // indirect
+	github.com/libp2p/go-libp2p-core v0.9.0 // indirect
+	github.com/multiformats/go-multiaddr v0.4.0 // indirect
 )
--- src/constants/constants.go
+++ src/constants/constants.go
@@ -1,7 +1,7 @@
 package constants
 
 const (
-	Lib_version = "v2.0.0"
+	Lib_version = "v2.2.0"
 	Lib_name = "libipfs"
 	Lib_usage = "A C-Style library implemented in Go for using IPFS in C++/C."
 	DefaultRepoPath = ".libipfs-repo"
@@ -25,4 +25,26 @@ var DefaultServerFilters = []string{
 	"/ip6/2001:db8::/ipcidr/32",
 	"/ip6/fc00::/ipcidr/7",
 	"/ip6/fe80::/ipcidr/10",
+}
+
+var BootstrapNodes = []string{
+	// IPFS Bootstrapper nodes.
+	"/dnsaddr/bootstrap.libp2p.io/p2p/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN",
+	"/dnsaddr/bootstrap.libp2p.io/p2p/QmQCU2EcMqAqQPR2i9bChDtGNJchTbq5TbXJJ16u19uLTa",
+	"/dnsaddr/bootstrap.libp2p.io/p2p/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb",
+	"/dnsaddr/bootstrap.libp2p.io/p2p/QmcZf59bWwK5XFi76CZX8cbJ4BhTzzA3gU1ZjYZcYW3dwt",
+
+	// IPFS Cluster Pinning nodes
+	"/ip4/138.201.67.219/tcp/4001/p2p/QmUd6zHcbkbcs7SMxwLs48qZVX3vpcM8errYS7xEczwRMA",
+	"/ip4/138.201.67.219/udp/4001/quic/p2p/QmUd6zHcbkbcs7SMxwLs48qZVX3vpcM8errYS7xEczwRMA",
+	"/ip4/138.201.67.220/tcp/4001/p2p/QmNSYxZAiJHeLdkBg38roksAR9So7Y5eojks1yjEcUtZ7i",
+	"/ip4/138.201.67.220/udp/4001/quic/p2p/QmNSYxZAiJHeLdkBg38roksAR9So7Y5eojks1yjEcUtZ7i",
+	"/ip4/138.201.68.74/tcp/4001/p2p/QmdnXwLrC8p1ueiq2Qya8joNvk3TVVDAut7PrikmZwubtR",
+	"/ip4/138.201.68.74/udp/4001/quic/p2p/QmdnXwLrC8p1ueiq2Qya8joNvk3TVVDAut7PrikmZwubtR",
+	"/ip4/94.130.135.167/tcp/4001/p2p/QmUEMvxS2e7iDrereVYc5SWPauXPyNwxcy9BXZrC1QTcHE",
+	"/ip4/94.130.135.167/udp/4001/quic/p2p/QmUEMvxS2e7iDrereVYc5SWPauXPyNwxcy9BXZrC1QTcHE",
+
+	// Scala IPFS nodes
+	"/ip4/135.181.166.154/tcp/4001/p2p/12D3KooWNKiAPbXoxjeNs5zVkHCCerYcUd1GShtETYtsc4W4qUYq",
+	"/ip4/135.181.166.154/udp/4010/quic/p2p/12D3KooWNKiAPbXoxjeNs5zVkHCCerYcUd1GShtETYtsc4W4qUYq",
 }
